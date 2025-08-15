1fed110f9ceef6db88f407b3e540972a02f34561
nazarbek <nazarbeknazarov772@gmail.com>
2025-08-16 00:06:47 +0500

    first version

--- bot_server.py
+++ bot_server.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 
 BOT_TOKEN = "8478003974:AAEf1RHpMJ8Aw0pAYx_Q2XtzbctAgY7V3EM"
-DJANGO_API_URL = "http://127.0.0.1:8000/api/save_chat_id/"
+DJANGO_API_URL = "https://restaranbasic-production.up.railway.app/api/save_chat_id/"
 DJANGO_API_TOKEN = "123"
 
 bot = Bot(token=BOT_TOKEN)
